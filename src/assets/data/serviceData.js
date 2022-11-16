1f59c4260d7168bdb5ae5cff953da60cf20b589c
AkiraZeZero <kiara.ocasio07@gmail.com>
2022-11-16 01:22:16 -0500

    adding UI/UX CSS

---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const serviceData = [
+    {
+        icon: "ri-truck-line",
+        title: "Free Shipping",
+        subtitle: "Free Standard Shipping on all orders",
+        bg: "#FFE186"
+    },
+    {
+        icon: "ri-refresh-line",
+        title: "Easy Returns",
+        subtitle: "Return new and unused items within 30 days",
+        bg: "rgb(147, 191, 230)"
+    },
+    {
+        icon: "ri-secure-payment-line",
+        title: "Secure Payment",
+        subtitle: "Secure Payments with Paypal",
+        bg: "#C5B0FF"
+    },
+    {
+        icon: "ri-user-smile-line",
+        title: "Satisfaction Guarantee",
+        subtitle: "You will love it!",
+        bg: "#8CC185"
+    },
+];
+
+export default serviceData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,58 @@
 import React from "react";
-import Helmet from "../components/Helmet/Helmet";
-import { Container, Row, Col } from "reactstrap";
-import hero from "../assets/images/epiphanyDisplay.jpg";
 import "../styles/home.css";
 
+import Helmet from "../components/Helmet/Helmet";
+import Services from "../components/services/Services";
+
+import hero from "../assets/images/epiphanyDisplay.jpg";
+
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 const Home = () => {
-    const year = new Date().getFullYear()
-    return (
-        <Helmet title={"Home"}>
-            <section className="hero__section">
-                <Container>
-                    <Row>
-                        <Col lg="6" md="6">
-                        <div>
-                            <p className="hero__subtitle">Trending Products in {year}</p>
-                            <h2>Make Your Skin More Naturally Beautiful</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita, laudantium soluta odit dolorem autem sunt illum velit ab cumque. Iure impedit voluptas error ad optio alias deleniti porro reprehenderit.</p>
-                            <button className="buy__btn">Shop Now</button>
-                        </div>
-                        </Col>
-                        <Col lg="6" md="6">
-                        <div>
-                            <img src={hero} alt="soap display" />
-                        </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </Helmet>
-    )
+  const year = new Date().getFullYear();
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div>
+                <p className="hero__subtitle">Trending Products in {year}</p>
+                <h2>Natural Skincare Just For You!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ratione expedita, laudantium soluta odit dolorem autem sunt
+                  illum velit ab cumque. Iure impedit voluptas error ad optio
+                  alias deleniti porro reprehenderit.
+                </p>
+                <motion.button whileHover={{ scale: 1.1 }} className="buy__btn">
+                  <Link to="/shop">Shop Now!</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div>
+                <img src={hero} alt="soap display" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Services/>
+      <section className="trending__products">
+        <Container>
+            <Row>
+                <Col lg="12" className="text__center">
+                <h2 className="section__title">Trending Products</h2>
+                </Col>
+            </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
